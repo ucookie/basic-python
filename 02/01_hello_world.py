b45331c4c6c8e1766782b45ac941d88f0cc89fa2
huangsihan <huangsihan@wps.cn>
2019-02-19 19:09:03 +0800

    提交部分文件

---
+++ 02/01_hello_world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('hello world')
+print('你好世界')
+
+# 单行注释使用 #
+# 多行注释使用三个单引号 ''' 或三个双引号 """ 将注释括起来
+
+'''
+多行注释
+print('hello world')
+print('你好世界')
+'''
+
+"""
+多行注释
+print('hello world')
+print('你好世界')
+"""
---
+++ 02/03_语法错误.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 1 语法错误：关键词使用错误
+priint('hello world')
+
+# 2 语法错误: 语法格式错误
+# while True
+#     print('Hello world')
