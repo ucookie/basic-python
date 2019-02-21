2493b2547c9deb3ace7f6d04c287bb7daf9a9ac4
huangsihan <huangsihan.cn@gmail.com>
2019-02-21 21:10:04 +0800

    第五章添加

---
+++ 04/01_列表的常见操作.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 1 定义一个列表
+a_list = list()
+a_list = []
+
+# 2 列表添加元素
+a_list.append('a')
+a_list.append('b')
+a_list.append('x')
+
+# 3 删除一个元素
+a_list.remove('x')
+
+# 4 查找元素
+a_list[0]
+a_list[1]
+
+# 5 修改元素值
+a_list[0] = 'z'
+a_list[1] = 'y'
