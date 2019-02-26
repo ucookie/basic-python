d20770b8ec1f4b9991ee359291b4dc9b8fbe920b
huangsihan <huangsihan.cn@gmail.com>
2019-02-26 19:50:51 +0800

    提交修改

--- 02/03_语法错误.py
+++ 02/03_语法错误.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # 1 语法错误：关键词使用错误
-priint('hello world')
+# priint('hello world')
 
 # 2 语法错误: 语法格式错误
-# while True
-#     print('Hello world')
+while True
+    print('Hello world')
