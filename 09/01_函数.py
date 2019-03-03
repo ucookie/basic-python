f4a804954e1825fd7da4f4f82ad077598c4e47e1
huangsihan <huangsihan.cn@gmail.com>
2019-03-03 11:46:12 +0800

    第9课

--- 09/01_函数.py
+++ 09/01_函数.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+def calc(numbers):
+    sum = 0
+    for n in numbers:
+        sum += n*n
+    return sum
+
+a_list = [1,2,3,4]
+calc(a_list)
+
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum=sum+n*n
+    return sum
+calc(1,2,3,4)
