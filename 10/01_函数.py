3a4efb4ecc30adc6c201220bef309ca0861a51b6
huangsihan <huangsihan@wps.cn>
2019-03-05 19:20:59 +0800

    新课

--- 10/01_函数.py
+++ 10/01_函数.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import pkg
+# print(pkg.name)
+
+from pkg import name
+# print(name)
---
+++ 10/pkg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+name = 'hello world'
+
+def printstr():
+    print('')
